3b6373cfed95dc14ad006672836d73f18c632168
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-03-27 14:55:53 -0600

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, buttonVariants } from "./components/ui/button/button"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<Button variant="outline">Button</Button>
+<Button variant="secondary">Button</Button>
+
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
