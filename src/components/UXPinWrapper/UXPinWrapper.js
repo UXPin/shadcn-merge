0a8b684516e32b36a0de81c6eff9e44168b49c79
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-03-24 10:44:21 -0600

    Added error display

--- src/components/UXPinWrapper/UXPinWrapper.js
+++ src/components/UXPinWrapper/UXPinWrapper.js
@@ -1,7 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import './App.css';
+import { ErrorBoundary } from 'react-error-boundary';
+
+function ErrorFallback({ error }) {
+  return (
+    <div style={{ color: 'red' }}>
+      <strong>ERROR:</strong> {error.message}
+    </div>
+  );
+}
 
 export default function UXPinWrapper({ children }) {
-  return children;
+  return (
+    <ErrorBoundary FallbackComponent={ErrorFallback}>{children}</ErrorBoundary>
+  );
 }
