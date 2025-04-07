ac005696d696d1ab7272fcfc47b699f250d16945
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2025-04-07 16:24:59 -0600

    Cleanup

--- uxpin.config.js
+++ uxpin.config.js
@@ -284,7 +284,6 @@ module.exports = {
       },
       Sheet: {
         open: { disabled: true, context: 'canvas', value: false },
-        defaultOpen: { disabled: true, context: 'canvas', value: false },
       },
     },
   },
