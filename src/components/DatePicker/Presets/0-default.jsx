1d52c52f190af509f60526d646bf0e9ba38c9b18
Robert Hilscher <r.hilscher@gmail.com>
2025-07-17 11:37:57 +0200

    fix presets

--- src/components/DatePicker/Presets/0-default.jsx
+++ src/components/DatePicker/Presets/0-default.jsx
@@ -1,15 +1,10 @@
 import * as React from 'react';
 import DatePicker from '../DatePicker';
 
-export default function DatePickerDefaultPreset() {
-  const [date, setDate] = React.useState(new Date());
-  return (
+export default (
     <DatePicker
       uxpId='datepicker-1'
-      date={date}
-      onDateChange={setDate}
       placeholder='Pick a date'
       width={240}
     />
-  );
-} 
+)
--- src/components/Tooltip/Presets/0-default.jsx
+++ src/components/Tooltip/Presets/0-default.jsx
@@ -4,8 +4,8 @@ import TooltipTrigger from '../../TooltipTrigger/TooltipTrigger';
 import TooltipContent from '../../TooltipContent/TooltipContent';
 
 export default (
-  <Tooltip>
-    <TooltipTrigger uxpId='tooltip-trigger'><span className='text-foreground'>Hover Me!</span></TooltipTrigger>
+  <Tooltip uxpId='tooltip'>
+    <TooltipTrigger uxpId='tooltip-trigger' className='text-foreground bg-background'>Hover Me!</TooltipTrigger>
     <TooltipContent uxpId='tooltip-content'>Add to library</TooltipContent>
   </Tooltip>
 );
