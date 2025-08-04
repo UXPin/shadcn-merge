0c3949fa76ee2a41bf1c3892bb7a08552176185e
Robert Hilscher <r.hilscher@gmail.com>
2025-08-04 14:47:35 +0200

    add sidebar to uxpinwrapper without any conditions

--- src/components/UXPinWrapper/UXPinWrapper.js
+++ src/components/UXPinWrapper/UXPinWrapper.js
@@ -12,57 +12,8 @@ function ErrorFallback({ error }) {
   );
 }
 
-
-function getComponentName(type) {
-  if (!type) return "";
-  return (
-      type.displayName || ''
-  );
-}
-
-function isSidebarComponentType(type, depth = 0) {
-  if (!type || depth > 5) return false; // zapobieganie nieskończonej rekurencji
-
-  const name = getComponentName(type);
-
-  if (name.includes("Sidebar")) {
-    return true;
-  }
-
-  return (
-      isSidebarComponentType(type.WrappedComponent, depth + 1) ||
-      isSidebarComponentType(type.render, depth + 1) ||
-      isSidebarComponentType(type.type, depth + 1)
-  );
-}
-
-function isSidebarComponent(component) {
-  return isSidebarComponentType(component?.type);
-}
-
-
-function containsSidebar(element) {
-  if (!element) return false;
-  if (isSidebarComponent(element)) return true;
-
-  const children = element.props?.children;
-  if (Array.isArray(children)) {
-    return children.some(containsSidebar);
-  } else if (typeof children === "object") {
-    return containsSidebar(children);
-  }
-
-  return false;
-}
-
 export default function UXPinWrapper({ children }) {
-  const shouldWrapWithSidebarProvider = containsSidebar(children);
-
-  const wrapped = shouldWrapWithSidebarProvider ? (
-      <SidebarProvider>{children}</SidebarProvider>
-  ) : (
-      children
+  return (
+      <ErrorBoundary FallbackComponent={ErrorFallback}><SidebarProvider>{children}</SidebarProvider></ErrorBoundary>
   );
-
-  return <ErrorBoundary FallbackComponent={ErrorFallback}>{wrapped}</ErrorBoundary>;
 }
