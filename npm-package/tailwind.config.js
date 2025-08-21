8746d968c93e5a0701cb5d119850c08c3613db0c
Robert Hilscher <r.hilscher@gmail.com>
2025-08-21 14:17:58 +0200

    fix tailwind.config

--- npm-package/tailwind.config.js
+++ npm-package/tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class'],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+  content:  [
+    '../src/**/*.{ts,tsx}',
   ],
   prefix: '',
   safelist: [
